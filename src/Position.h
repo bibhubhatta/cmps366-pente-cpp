c8b44a20e19f0983f7f6b92af2a3fda48a77dcf0
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-09-11 00:15:59 -0400

    Add header guard and reformat

--- src/Position.h
+++ src/Position.h
@@ -1,10 +1,10 @@
+#pragma once
+
 #include <string>
 
 /// \brief Represents a position on the board
-class Position
-
-{
-  public:
+class Position {
+public:
     /// \brief Represents a position on the board
     /// \param row
     /// \param col
@@ -13,19 +13,21 @@ class Position
     /// \brief Creates a Position from a string of the form
     /// "<col_char><row_int>"
     /// \param position_string
-    Position(const std::string& position_string);
+    Position(const std::string &position_string);
 
-    Position(const Position& other) = default;
+    Position(const Position &other) = default;
 
     const int row;
     const int col;
 
     std::string to_string() const;
 
-    bool operator==(const Position& other) const;
-    bool operator!=(const Position& other) const;
+    bool operator==(const Position &other) const;
 
-  private:
-    static int get_row_number_from_position_string(const std::string& position);
-    static int get_col_number_from_position_string(const std::string& position);
+    bool operator!=(const Position &other) const;
+
+private:
+    static int get_row_number_from_position_string(const std::string &position);
+
+    static int get_col_number_from_position_string(const std::string &position);
 };
