e9d57fe9266aedf8256677b3781b584991cafbfc
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-09-23 23:30:27 +0000

    Format bottom and top rows

--- src/UnicodeDisplayHelpers.cpp
+++ src/UnicodeDisplayHelpers.cpp
@@ -8,4 +8,78 @@ void use_unicode_borders(tabulate::Table& table)
         .border_left("│")
         .border_right("│")
         .corner("┼");
+
+    auto no_rows = table.size();
+    int  row_index = 0;
+
+    for (auto& row : table)
+    {
+        if (row_index == 0)
+        {
+            format_top_row(row);
+        }
+
+        if (row_index == no_rows - 1)
+        {
+            format_bottom_row(row);
+        }
+
+        row_index++;
+    }
+}
+
+void format_top_row(tabulate::Row row)
+{
+    row.format().border_top("─").border_bottom("─");
+
+    auto no_columns = row.size();
+    int  column_index = 0;
+
+    for (auto& column : row)
+    {
+        if (column_index == 0)
+        {
+            column.format().corner_top_left("┌").corner_top_right("┬");
+        }
+
+        if (column_index == no_columns - 1)
+        {
+            column.format().corner_top_left("┬").corner_top_right("┐");
+        }
+
+        if (column_index > 0 && column_index < no_columns - 1)
+        {
+            column.format().corner_top_left("┬").corner_top_right("┬");
+        }
+
+        column_index++;
+    }
+}
+
+void format_bottom_row(tabulate::Row row)
+{
+    row.format().border_top("─").border_bottom("─");
+
+    auto no_columns = row.size();
+    int  column_index = 0;
+
+    for (auto& column : row)
+    {
+        if (column_index == 0)
+        {
+            column.format().corner_bottom_left("└").corner_bottom_right("┴");
+        }
+
+        if (column_index == no_columns - 1)
+        {
+            column.format().corner_bottom_right("┘").corner_bottom_left("┴");
+        }
+
+        if (column_index > 0 && column_index < no_columns - 1)
+        {
+            column.format().corner_bottom_left("┴").corner_bottom_right("┴");
+        }
+
+        column_index++;
+    }
 }
--- src/UnicodeDisplayHelpers.h
+++ src/UnicodeDisplayHelpers.h
@@ -3,3 +3,6 @@
 #include <tabulate/table.hpp>
 
 void use_unicode_borders(tabulate::Table& table);
+
+void format_top_row(tabulate::Row row);
+void format_bottom_row(tabulate::Row row);
